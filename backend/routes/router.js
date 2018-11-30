727ef6811258afa8bfe8f638e65a6abc673ddcf4
condrint <condrint@gmail.com>
2018-11-29 20:34:43 -0800

    working on peers

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -4,7 +4,15 @@ const express = require('express');
 // get an instance of express router
 const router = express();
 
-router.post('/join', {});
+var start = true;
+
+router.post('/join', (req, res) => {
+    let temp = start;
+    start = false;
+    return res.status(200).json({
+        isInitator: temp
+    })
+});
 router.post('/leave', {});
 
 module.exports = router;
--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,45 @@
 import React, { Component } from 'react';
+import Peer from 'simple-peer';
+import axios from 'axios';
+
+var peer = null;
 
 class App extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      input: ''
+    }
+    this.join = this.join.bind(this);
+    this.leave = this.leave.bind(this);
+    this.change = this.change.bind(this);
+  }
+  
+  change(e){
+    this.setState({input:e.target.value});
+  }
+
+  async join(){
+    try {
+      var peerConnectionInfo = await axios.get('/api/join');
+    } catch(e){
+      console.error(e);
+    }
+    let isInitiator = peerConnectionInfo.data.isInitiator;
+    peer = new Peer({initiator: isInitiator});
+  }
+
+  leave(){
+
+  }
+
   render() {
     return (
       <div className="App">
+          <input value={this.state.input} onChange={this.change}/>
+          <button onClick={this.join}>Join</button>
+          <button onClick={this.leave}>Leave</button>
       </div>
     );
   }
